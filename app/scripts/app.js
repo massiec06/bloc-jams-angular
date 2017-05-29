8f82be519538abb53b9637324447e1357a21d6b6
Chris Massie <massiec06@gmail.com>
2017-05-28 23:03:56 -0400

    assignment 5 controllers: completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,6 +14,7 @@
          })
          .state('album', {
              url: '/album',
+             controller: 'AlbumCtrl as AlbumCtrl',
              templateUrl: '/templates/album.html'
          })
          .state('collection', {
