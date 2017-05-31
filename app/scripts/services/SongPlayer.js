83fa842945eaf151cc008e4a8ee12cdd5036fdc7
Chris Massie <massiec06@gmail.com>
2017-05-31 10:38:31 -0400

    assignment 7 Services Pt2: completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,42 +1,81 @@
 (function() {
+     /**
+     *@function SongPlayer
+     *@desc Plays, pauses, and changes songs on album's page
+     *@param {Object}
+     **/
+
      function SongPlayer() {
           var SongPlayer = {};
 
+
           var currentSong = null;
+
+          /**
+          * @desc Buzz object audio file
+          * @type {Object}
+          */
+
           var currentBuzzObject = null;
 
+          /**
+          *@function playSong
+          *@desc Sets current Buzz object to play and song playing to true
+          *@param {Object} song
+          **/
+
+          var playSong = function(song) {
+              currentBuzzObject.play();
+              song.playing = true;
+          }
+
+          /**
+          * @function setSong
+          * @desc Stops currently playing song and loads new audio file as currentBuzzObject
+          * @param {Object} song
+          */
+
           var setSong = function(song) {
-              if (currentBuzzObject) {
-                  currentBuzzObject.stop();
-                  currentSong.playing = null;
-              }
+            if (currentBuzzObject) {
+                currentBuzzObject.stop();
+                currentSong.playing = null;
+            }
 
-              currentBuzzObject = new buzz.sound(song.audioUrl, {
-                  formats: ['mp3'],
-                  preload: true
-              });
+            currentBuzzObject = new buzz.sound(song.audioUrl, {
+                formats: ['mp3'],
+                preload: true
+            });
 
-              currentSong = song;
-          };
+            currentSong = song;
+         };
+
+         /**
+         *@function SongPlayer.play
+         *@desc Sets the song to play if current song is not set to a song else if current song is set to song checks if paused before setting Buzz Object to play
+         *@param {Object} song
+         **/
 
           SongPlayer.play = function(song) {
-            console.log(song)
-             if (currentSong !== song) {
+              if (currentSong !== song) {
                 setSong(song);
-                currentBuzzObject.play();
-                song.playing = true;
-
-             } else if (currentSong === song) {
-                if (currentBuzzObject.isPaused()) {
-                  currentBuzzObject.play();
-                }
-             }
+                playSong(song);
+              } else if (currentSong === song) {
+                  if (currentBuzzObject.isPaused()) {
+                      currentBuzzObject.play();
+                  }
+              }
           };
 
-          SongPlayer.pause = function(song) {
-              currentBuzzObject.pause();
-              song.playing = false;
-          };
+         /**
+         *@function SongPlayer.pause
+         *@desc pauses current audio file and sets song playing to false
+         *@param {Object} song
+         **/
+         
+         SongPlayer.pause = function(song) {
+            currentBuzzObject.pause();
+            song.playing = false;
+        };
 
           return SongPlayer;
      }
